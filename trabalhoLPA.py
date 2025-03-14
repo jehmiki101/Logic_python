52e91bab50fbe77cd38814a2e69ca3af25ab4b74
Jéssica <jmikihara@gmail.com>
2025-03-14 20:16:21 -0300

    Primeira parte do trabalho

---
+++ trabalhoLPA.py
@@ -0,0 +1,18 @@
+# Questão 01
+
+# A
+print('Bem-vindo a Loja da Jéssica Hara')
+# B
+valor_unitario = int(input('Entre com o valor do produto: '))
+quantidade = int(input('Entre com a quantidade do produto: '))
+# C
+total = valor_unitario * quantidade
+if (total < 2500):
+
+elif (total >= 2500 or total < 6000 ):
+
+elif (total >= 6000 or total < 10000):
+
+else:
+
+    
