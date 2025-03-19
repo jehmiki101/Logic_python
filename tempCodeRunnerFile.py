d2c1cc39dcffe24fe376561fb3e1bf4335557caf
Jéssica <jmikihara@gmail.com>
2025-03-19 15:58:52 -0300

    término segundo exercício do trabalho

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,65 @@
+# Questão 02
 
-print('=' * 30)
-print(' ' * 7 + 'JOGO DA FORCA')
-print('=' * 30)
-print('\n1 - JOGAR')
-print('2 - SCORE')
-print('3 - SAIR\n')
-print('=' * 30)
+# A - Print com Boas vindas e cardápio
+print('Bem-vindo a Loja De açaí e cupuaçu da Jéssica Hara!')
+print('-' * 20, 'Cardápio', '-' * 20)
+print('-' * 50)
+print('---', '|', ' Tamanho ', '|', ' Cupuaçu(CP) ', '|', ' Açaí(AC) ', '|', '---')
+print('---', '|', '    P    ', '|', '  R$  9,00   ', '|', ' R$ 11,00 ', '|', '---')
+print('---', '|', '    M    ', '|', '  R$ 14,00   ', '|', ' R$ 16,00 ', '|', '---')
+print('---', '|', '    G    ', '|', '  R$ 18,00   ', '|', ' R$ 20,00 ', '|', '---')
+print('-' * 50)
+
+def pedido ():
+    while True:# D, G - Implementação de if/elif/else em modelo aninhado e While, break, continue
+        valor_unidade = 0
+        quantidade = 0
+        global total# E - Acumulador para somatória de todos os valores
+
+        sabor = input('Entre com o sabor desejado (CP ou AC): ')# Input sabor e validação
+        if (sabor != 'CP' and sabor != 'AC'):
+            print('Sabor inválido. Tente novamente', end='')
+            continue
+
+        tamanho = input('Entre com o tamanho desejado (P/M/G): ')# Input tamanho e validação
+        if (tamanho != 'P' and tamanho != 'M' and tamanho != 'G'):
+            print('Tamanho inválido. Tente novamente')
+            continue
+
+        if (sabor == 'CP'):# B - Escolha do sabor Cupuaçu
+            if (tamanho == 'P'):# C - Escolha de tamanho P para Cupuaçu
+                valor_unidade = 9
+                print(f'Você pediu um Cupuaçu no tamanho P: R$', valor_unidade)
+                total += valor_unidade
+            elif (tamanho == 'M'):# C - Escolha de tamanho M para Cupuaçu
+                valor_unidade = 14
+                print(f'Você pediu um Cupuaçu no tamanho M: R$', valor_unidade)
+                total += valor_unidade
+            else:# C - Escolha de tamanho G para Cupuaçu
+                valor_unidade = 18
+                print(f'Você pediu um Cupuaçu no tamanho G: R$', valor_unidade)
+                total += valor_unidade
+        elif (sabor == 'AC'):# B - Escolha do sabor Açai
+            if (tamanho == 'P'):# C - Escolha de tamanho P para Açai
+                valor_unidade = 11
+                print(f'Você pediu um Açaí no tamanho P: R$', valor_unidade)
+                total += valor_unidade
+            elif (tamanho == 'M'):# C - Escolha de tamanho M para Açai
+                valor_unidade = 16
+                print(f'Você pediu um Açaí no tamanho M: R$', valor_unidade)
+                total += valor_unidade
+            else:# C - Escolha de tamanho G para Açai
+                valor_unidade = 20
+                print(f'Você pediu um Açaí no tamanho G: R$', valor_unidade)
+                total += valor_unidade
+        break
+
+total = 0
+pedido()
+adicao_pedido = input('Deseja mais alguma coisa? (S/N)')# F - Input adição de pedido
+while (adicao_pedido == 'N'):
+    break
+while (adicao_pedido == 'S'):
+    pedido()
+    break
+print(f'O valor total a ser pago: R${total}')
--- trabalhoLPA.py
+++ trabalhoLPA.py
@@ -36,53 +36,56 @@ print('---', '|', '    M    ', '|', '  R$ 14,00   ', '|', ' R$ 16,00 ', '|', '--
 print('---', '|', '    G    ', '|', '  R$ 18,00   ', '|', ' R$ 20,00 ', '|', '---')
 print('-' * 50)
 
-# B - Input de sabor
-
 def pedido ():
-    while True:
-        sabor = input('Entre com o sabor desejado (CP ou AC): ')
+    while True:# D, G - Implementação de if/elif/else em modelo aninhado e While, break, continue
         valor_unidade = 0
         quantidade = 0
-        total = 0
-        if (sabor == 'CP'):
-            tamanho = input('Entre com o tamanho desejado (P/M/G): ')
-            if (tamanho == 'P'):
-                valor_unidade = 9
-                quantidade += 1
-                print(f'Você pediu um Cupuaçu no tamanho P: R$', valor_unidade)
-            elif (tamanho == 'M'):
-                valor_unidade = 14
-                quantidade += 1
-                print(f'Você pediu um Cupuaçu no tamanho M: R$', valor_unidade)
-            elif (tamanho == 'G'):
-                valor_unidade = 18
-                quantidade += 1
-                print(f'Você pediu um Cupuaçu no tamanho G: R$', valor_unidade)
-            else:
-                print('Tamanho inválido, tente novamente')
-                break
-        elif (sabor == 'AC'):
-            tamanho = input('Entre com o tamanho desejado (P/M/G): ')
-            if (tamanho == 'P'):
-                valor_unidade = 11
-                quantidade += 1
-                print(f'Você pediu um Açaí no tamanho P: R$', valor_unidade)
-            elif (tamanho == 'M'):
-                valor_unidade = 16
-                quantidade += 1
-                print(f'Você pediu um Açaí no tamanho M: R$', valor_unidade)
-            elif (tamanho == 'G'):
-                valor_unidade = 20
-                quantidade += 1
-                print(f'Você pediu um Açaí no tamanho G: R$', valor_unidade)
-            else:
-                print('Tamanho inválido, tente novamente')
-                break
-        else:
+        global total# E - Acumulador para somatória de todos os valores
+
+        sabor = input('Entre com o sabor desejado (CP ou AC): ')# Input sabor e validação
+        if (sabor != 'CP' and sabor != 'AC'):
             print('Sabor inválido. Tente novamente')
-        total += valor_unidade
+            continue
+
+        tamanho = input('Entre com o tamanho desejado (P/M/G): ')# Input tamanho e validação
+        if (tamanho != 'P' and tamanho != 'M' and tamanho != 'G'):
+            print('Tamanho inválido. Tente novamente')
+            continue
+
+        if (sabor == 'CP'):# B - Escolha do sabor Cupuaçu
+            if (tamanho == 'P'):# C - Escolha de tamanho P para Cupuaçu
+                valor_unidade = 9
+                print(f'Você pediu um Cupuaçu no tamanho P: R$', valor_unidade)
+                total += valor_unidade
+            elif (tamanho == 'M'):# C - Escolha de tamanho M para Cupuaçu
+                valor_unidade = 14
+                print(f'Você pediu um Cupuaçu no tamanho M: R$', valor_unidade)
+                total += valor_unidade
+            else:# C - Escolha de tamanho G para Cupuaçu
+                valor_unidade = 18
+                print(f'Você pediu um Cupuaçu no tamanho G: R$', valor_unidade)
+                total += valor_unidade
+        elif (sabor == 'AC'):# B - Escolha do sabor Açai
+            if (tamanho == 'P'):# C - Escolha de tamanho P para Açai
+                valor_unidade = 11
+                print(f'Você pediu um Açaí no tamanho P: R$', valor_unidade)
+                total += valor_unidade
+            elif (tamanho == 'M'):# C - Escolha de tamanho M para Açai
+                valor_unidade = 16
+                print(f'Você pediu um Açaí no tamanho M: R$', valor_unidade)
+                total += valor_unidade
+            else:# C - Escolha de tamanho G para Açai
+                valor_unidade = 20
+                print(f'Você pediu um Açaí no tamanho G: R$', valor_unidade)
+                total += valor_unidade
         break
 
+total = 0
 pedido()
-adicao_pedido = input('Deseja mais alguma coisa? (S/N)')
-pedido()
+adicao_pedido = input('Deseja mais alguma coisa? (S/N)')# F - Input adição de pedido
+while (adicao_pedido == 'N'):
+    break
+while (adicao_pedido == 'S'):
+    pedido()
+    break
+print(f'O valor total a ser pago: R${total}')
